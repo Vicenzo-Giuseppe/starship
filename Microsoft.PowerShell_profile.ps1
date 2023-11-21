bb1ff312423b3100a09855d2aec6b9e1572f1312
Vicenzo Giuseppe <vicenzogiuseppe@gmail.com>
2023-11-21 03:07:57 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 $ENV:STARSHIP_CONFIG = "$HOME\scoop\apps\starship\current\prompt.toml"
+Set-Alias -Name lvim -Value C:\Users\Vicen\.local\bin\lvim.ps1
+Set-Alias -Name c -Value clear
 Invoke-Expression (&starship init powershell)
